9b2c45a65753ca12ec16dc9e42c12f2cd83d66fe
Christoph <christoph1009@gmail.com>
2024-09-27 20:47:29 +0000

    adapted biome rules to be nicer looking for me

--- src/pi_cycle.ts
+++ src/pi_cycle.ts
@@ -1,21 +1,21 @@
-import Chart from "chart.js/auto";
-import zoomPlugin from "chartjs-plugin-zoom";
-Chart.register(zoomPlugin);
+import Chart from 'chart.js/auto'
+import zoomPlugin from 'chartjs-plugin-zoom'
+Chart.register(zoomPlugin)
 
-import btc_data from "../data/btc_prices_until_2023.json";
+import btc_data from '../data/btc_prices_until_2023.json'
 
 export interface PriceData {
-  date: string;
-  price?: number;
+  date: string
+  price?: number
 }
 
-(async () => {
-  const chartElement = document.getElementById("pi_cycle");
+;(async () => {
+  const chartElement = document.getElementById('pi_cycle')
   if (chartElement == null) {
-    return;
+    return
   }
 
-  btc_data.reverse(); // Just sort the initial file correctly.
+  btc_data.reverse() // Just sort the initial file correctly.
 
   const calculateMovingAverage = (
     data: PriceData[],
@@ -25,43 +25,43 @@ export interface PriceData {
     // Clone the input dates but set the price to undefined.
     const out: PriceData[] = Object.values(structuredClone(data)).map(
       (entry) => {
-        return { date: entry.date, price: undefined };
+        return { date: entry.date, price: undefined }
       },
-    );
+    )
     if (data.length < window || window <= 0) {
-      return out;
+      return out
     }
-    let sum = 0;
+    let sum = 0
     for (let i = 0; i < window; i++) {
-      sum += data[i].price ?? 0;
+      sum += data[i].price ?? 0
     }
-    out[window - 1].price = (sum / window) * factor;
+    out[window - 1].price = (sum / window) * factor
     for (let i = window; i < data.length; i++) {
-      sum += (data[i].price ?? 0) - (data[i - window].price ?? 0);
-      out[i].price = (sum / window) * factor;
+      sum += (data[i].price ?? 0) - (data[i - window].price ?? 0)
+      out[i].price = (sum / window) * factor
     }
-    return out;
-  };
+    return out
+  }
 
-  const mva_350 = calculateMovingAverage(btc_data, 350, 2);
-  const mva_111 = calculateMovingAverage(btc_data, 111);
+  const mva_350 = calculateMovingAverage(btc_data, 350, 2)
+  const mva_111 = calculateMovingAverage(btc_data, 111)
 
   new Chart(chartElement as HTMLCanvasElement, {
-    type: "line",
+    type: 'line',
     data: {
       labels: btc_data.map((row) => row.date),
       datasets: [
         {
-          label: "btc price",
+          label: 'btc price',
           data: btc_data,
           borderWidth: 1,
         },
         {
-          label: "350 mva x 2",
+          label: '350 mva x 2',
           data: mva_350,
         },
         {
-          label: "111 mva",
+          label: '111 mva',
           data: mva_111,
         },
       ],
@@ -75,12 +75,12 @@ export interface PriceData {
         },
       },
       parsing: {
-        xAxisKey: "date",
-        yAxisKey: "price",
+        xAxisKey: 'date',
+        yAxisKey: 'price',
       },
       scales: {
         y: {
-          type: "logarithmic",
+          type: 'logarithmic',
         },
       },
       plugins: {
@@ -92,10 +92,10 @@ export interface PriceData {
             pinch: {
               enabled: true,
             },
-            mode: "xy",
+            mode: 'xy',
           },
         },
       },
     },
-  });
-})();
+  })
+})()
